585a3edbf98ee91887aa10cc2e5f5fac9fe9d158
Moni Rivas <mrivaso1900@alumno.ipn.mx>
2024-10-26 01:20:32 -0600

    Seccion de camara funcional, toma foto y la guarda en una variable
    barra de navegación en la parte inferior que nos permite ir de las listas de productos a la camara
    
    aun no se implementa la lista de productos

--- app/src/main/java/com/example/aiuda/MainActivity.kt
+++ app/src/main/java/com/example/aiuda/MainActivity.kt
@@ -1,20 +1,71 @@
 package com.example.aiuda
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.os.Build
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.provider.MediaStore
+import android.widget.ImageView
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import fragments.HomeFragment
+import fragments.ProfileFragment
 
 class MainActivity : AppCompatActivity() {
+
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        // Listener para manejar la selección de items en la barra de navegación
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> {
+                    loadFragment(HomeFragment())
+                    true
+                }
+
+                R.id.nav_profile -> {
+                    openCamera()
+                    true
+                }
+
+                else -> false
+            }
         }
+        // Cargar el fragmento inicial
+        bottomNavigationView.selectedItemId = R.id.nav_home
     }
+
+    // Función para reemplazar el fragmento actual
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, fragment)
+            .commit()
+    }
+
+    // Función para abrir la cámara
+    private fun openCamera() {
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startForResult.launch(cameraIntent)
+    }
+
+    // Evento que procesa el resultado de la cámara y envía la vista previa de la foto al ImageView
+    private val startForResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val intent = result.data
+                val imageBitmap = intent?.extras?.get("data") as Bitmap
+                val imageView = findViewById<ImageView>(R.id.imageView)
+                imageView.setImageBitmap(imageBitmap)
+            }
+        }
 }
